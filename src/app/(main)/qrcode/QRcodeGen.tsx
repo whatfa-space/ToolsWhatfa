fa4ca9e7717d045a66015ef6ecb83feb279dce50
mark <wsjzs@hotmail.com>
2023-07-08 04:39:57 +0800

    feat: qrcode

--- src/app/(main)/qrcode/QRcodeGen.tsx
+++ src/app/(main)/qrcode/QRcodeGen.tsx
@@ -7,17 +7,22 @@ import { FormEvent, useCallback, useState } from 'react'
 export const QRcodeGen = () => {
   const { Image: QrCodeImage } = useQRCode()
   const [text, setText] = useState('https://tools.whatfa.com/')
+  const [inputText, setInputText] = useState('https://tools.whatfa.com/')
 
   const handleInput = useCallback((event: FormEvent) => {
     const value = getInputValue(event)
-    setText(value)
+    setInputText(value)
   }, [])
 
+  const handleGen = useCallback(() => {
+    setText(inputText)
+  }, [inputText])
+
   return (
-    <div className='flex flex-col items-center md:flex-row md:min-h-[200px]'>
-      <div className='flex flex-col my-3 md:mr-10 w-full'>
-        <textarea value={text} className='textarea textarea-bordered w-full' onInput={handleInput} placeholder='请输入网址 https://xx.xxx'></textarea>
-        <button className='btn btn-sm btn-outline my-6'>生成二维码</button>
+    <div className='flex flex-col items-center md:flex-row md:min-h-[200px] justify-center'>
+      <div className='flex flex-col my-3 md:mr-10 w-full max-w-lg'>
+        <textarea value={inputText} className='textarea textarea-bordered w-full' onInput={handleInput} placeholder='请输入网址 https://xx.xxx 或任意文本'></textarea>
+        <button onClick={handleGen} disabled={inputText.length <= 0} className='btn btn-sm btn-outline my-6'>生成二维码</button>
       </div>
       <div className='border shadow'>
         <QrCodeImage
