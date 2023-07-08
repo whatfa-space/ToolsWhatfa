724bd0943b8f5dc22339af3ec30660f07f882b9d
Jason <17230175+wsjzs@users.noreply.github.com>
2023-07-08 08:10:33 +0000

    feat: qrcode download

--- src/app/(main)/qrcode/QRcodeGen.tsx
+++ src/app/(main)/qrcode/QRcodeGen.tsx
@@ -2,12 +2,13 @@
 
 import { getInputValue } from '@/utils'
 import { useQRCode } from 'next-qrcode'
-import { FormEvent, useCallback, useState } from 'react'
+import { FormEvent, useCallback, useRef, useState } from 'react'
 
 export const QRcodeGen = () => {
   const { Image: QrCodeImage } = useQRCode()
   const [text, setText] = useState('https://tools.whatfa.com/')
   const [inputText, setInputText] = useState('https://tools.whatfa.com/')
+  const codeImgRef = useRef()
 
   const handleInput = useCallback((event: FormEvent) => {
     const value = getInputValue(event)
@@ -18,11 +19,16 @@ export const QRcodeGen = () => {
     setText(inputText)
   }, [inputText])
 
+  const handleDownload = () => {
+    alert('下载功能暂未开发')
+  }
+
   return (
     <div className='flex flex-col items-center md:flex-row md:min-h-[200px] justify-center'>
       <div className='flex flex-col my-3 md:mr-10 w-full max-w-lg'>
         <textarea value={inputText} className='textarea textarea-bordered w-full' onInput={handleInput} placeholder='请输入网址 https://xx.xxx 或任意文本'></textarea>
-        <button onClick={handleGen} disabled={inputText.length <= 0} className='btn btn-sm btn-outline my-6'>生成二维码</button>
+        <button onClick={handleGen} disabled={inputText.length <= 0} className='btn btn-sm btn-outline mt-6'>生成二维码</button>
+        <button onClick={handleDownload} className='btn btn-sm btn-outline mt-6'>下载</button>
       </div>
       <div className='border shadow'>
         <QrCodeImage
