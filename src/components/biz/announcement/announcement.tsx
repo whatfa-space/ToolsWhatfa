ff63e66878564973e0a544217b3c73c8eb581b54
marx <wsjzs@hotmail.com>
2023-07-15 12:53:39 +0800

    feat: target _blank

--- src/components/biz/announcement/announcement.tsx
+++ src/components/biz/announcement/announcement.tsx
@@ -4,12 +4,17 @@ import { FaIcon } from '../FaIcon'
 
 // FaIcon
 export function Announcement() {
-  return (<TagContainer title={'公告'} >
-    <ul>
-      <li>
-        <span><FaIcon icon='faUserGroup' className='w-5 h-5 inline'/> QQ闲聊群<Link href={'https://qm.qq.com/q/xWyvsLDuIE'}>496313529</Link></span>
-      </li>
-    </ul>
-
-  </TagContainer>)
+  return (
+    <TagContainer title={'公告'}>
+      <ul>
+        <li>
+          <span>
+            <FaIcon icon="faUserGroup" className="w-5 h-5 inline" />
+              QQ闲聊群
+            <Link target='_blank' href={'https://qm.qq.com/q/xWyvsLDuIE'}>496313529</Link>
+          </span>
+        </li>
+      </ul>
+    </TagContainer>
+  )
 }
