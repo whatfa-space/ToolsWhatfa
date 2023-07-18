6d9101782b0cb422f633d97edb3480a20073544b
mark <wsjzs@hotmail.com>
2023-07-18 13:31:19 +0800

    feat: duration

--- src/components/ui/TLink/t-link.tsx
+++ src/components/ui/TLink/t-link.tsx
@@ -11,7 +11,7 @@ export default function TLink({ children, className, ...setup }: TLinkProps) {
     <>
       <Link
         className={clsxm(
-          'text-info relative before:content-[""] before:absolute before:w-0 before:bg-info before:h-[2px] before:bottom-0  hover:before:w-full  before:transition-all  before:duratioin-300 before:rounded',
+          'text-info relative before:content-[""] before:absolute before:w-0 before:bg-info before:h-[2px] before:bottom-0  hover:before:w-full  before:transition-all  before:duration-300 before:rounded',
           className,
         )}
         {...setup}
