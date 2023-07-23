728621ef5b6c1a15f62056fc67697947c6f909dd
mark <wsjzs@hotmail.com>
2023-07-23 21:19:11 +0800

    feat: footer link

--- src/components/layout/footer/footer.tsx
+++ src/components/layout/footer/footer.tsx
@@ -1,8 +1,11 @@
 import { ThemeSwitch } from '@/components/common/theme-switch'
+import Link from 'next/link'
 
 export const Footer = () => (
   <footer className="p-6 border-t border-zinc-200/80 flex flex-col items-center">
-    <div className="mb-3">© 2020-2023 whatfa</div>
+    <div className="mb-3">
+      © 2020-2023 <Link href="https://tools.whatfa.com">tools.whatfa.com</Link>
+    </div>
     <ThemeSwitch />
   </footer>
 )
