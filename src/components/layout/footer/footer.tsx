c543f01f55c57387ab75ea8372ac9075f715016e
mark <wsjzs@hotmail.com>
2023-07-23 22:23:23 +0800

    feat: foot mt

--- src/components/layout/footer/footer.tsx
+++ src/components/layout/footer/footer.tsx
@@ -2,7 +2,7 @@ import { ThemeSwitch } from '@/components/common/theme-switch'
 import Link from 'next/link'
 
 export const Footer = () => (
-  <footer className="p-6 border-t border-zinc-200/80 flex flex-col items-center">
+  <footer className="p-6 border-t border-zinc-200/80 flex flex-col items-center mt-32">
     <div className="mb-3">
       © 2020-2023 <Link href="https://tools.whatfa.com">tools.whatfa.com</Link>
     </div>
