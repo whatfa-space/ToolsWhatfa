3df75487f0096224271c1348c2e6933f7af166ab
mark <wsjzs@hotmail.com>
2023-07-29 13:21:09 +0800

    feat: contact config

--- src/components/layout/footer/Contact.tsx
+++ src/components/layout/footer/Contact.tsx
@@ -2,16 +2,28 @@ import GithubIcon from '@/components/icon/github-icon'
 import TLink from '@/components/ui/TLink/t-link'
 
 const Contact = () => {
+  const contactList = [
+    {
+      key: 'GITHUB',
+      tooltip: 'GitHub',
+      href: 'https://github.com/whatfa-space/ToolsWhatfa',
+      component: <GithubIcon className="w-7 h-7" />,
+    },
+  ]
   return (
-    <div className="flex flex-row items-center mr-2">
-      <TLink
-        href="https://github.com/whatfa-space/ToolsWhatfa"
-        target="_blank"
-        underline={false}
-        className="s shadow-xl rounded-full"
-      >
-        <GithubIcon className="w-7 h-7" />
-      </TLink>
+    <div className="flex flex-row items-center mr-4">
+      {contactList.map((item) => (
+        <div key={item.key} className="tooltip" data-tip={item.tooltip}>
+          <TLink
+            href={item.href}
+            target="_blank"
+            underline={false}
+            className="s shadow-xl rounded-full"
+          >
+            {item.component}
+          </TLink>
+        </div>
+      ))}
     </div>
   )
 }
