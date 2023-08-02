7d6013e796be7263a7ed6188e42248d8688bc8fc
mark <wsjzs@hotmail.com>
2023-08-02 09:59:23 +0800

    feat: style footer

--- src/components/layout/footer/footer.tsx
+++ src/components/layout/footer/footer.tsx
@@ -6,7 +6,11 @@ export const Footer = () => (
   <footer className="p-6 border-t border-zinc-200/80 flex flex-col items-center">
     <div className="mb-3">
       © 2020-2023 &nbsp;
-      <Link href="https://tools.whatfa.com" target="_blank">
+      <Link
+        href="https://tools.whatfa.com"
+        target="_blank"
+        className="underline underline-offset-4"
+      >
         tools.whatfa.com
       </Link>
     </div>
