149444d97d6edb4f6cf77b6fa680e2bac44adba4
mark <wsjzs@hotmail.com>
2023-08-02 10:32:18 +0800

    feat: clipboard dynamic auto

--- src/app/(main)/clipboard/page.tsx
+++ src/app/(main)/clipboard/page.tsx
@@ -1,8 +1,10 @@
-import { awaitGenerateRandomString } from '@/utils'
+import { generateRandomString } from '@/utils'
 import InputId from './InputId'
 
-export default async function Clipboard() {
-  const initId = await awaitGenerateRandomString(4)
+export const dynamic = 'auto'
+
+export default function Clipboard() {
+  const initId = generateRandomString(4)
   return (
     <>
       <p className="text-center text-2xl">在线剪切板 内容跨端分享</p>
