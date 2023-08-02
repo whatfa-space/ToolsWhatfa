2c829ac92eb8a46b5bba65711d1575fb43693dcc
mark <wsjzs@hotmail.com>
2023-08-02 15:12:18 +0800

    feat: no random

--- src/services/clipboard/index.ts
+++ src/services/clipboard/index.ts
@@ -6,13 +6,10 @@ interface ClipboardRes {
 }
 
 export async function getClipboard(id: string): Promise<ClipboardRes> {
-  const res = await fetch(
-    url('/api/clipboard?id=' + id + '&r=' + Math.random()),
-    {
-      method: 'GET',
-      cache: 'no-cache',
-    }
-  )
+  const res = await fetch(url('/api/clipboard?id=' + id), {
+    method: 'GET',
+    cache: 'no-cache',
+  })
   const { data } = await res.json()
   return data
 }
