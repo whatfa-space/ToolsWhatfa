c6018b5c5d6e2c7a2f35c657ba4598a277062688
mark <wsjzs@hotmail.com>
2023-08-02 17:35:45 +0800

    feat: baidu src

--- src/components/baidu/index.tsx
+++ src/components/baidu/index.tsx
@@ -8,14 +8,19 @@ export default function Baidu() {
   //   s.parentNode.insertBefore(hm, s);
   // })();
   // `
-  const hm = `
-  var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?f09245f6946d90bfd6d5cd6a0eed11c6";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
-  `
-  return <script dangerouslySetInnerHTML={{ __html: hm }}></script>
+  // const hm = `
+  //   var _hmt = _hmt || [];
+  // (function() {
+  //   var hm = document.createElement("script");
+  //   hm.src = "https://hm.baidu.com/hm.js?f09245f6946d90bfd6d5cd6a0eed11c6";
+  //   var s = document.getElementsByTagName("script")[0];
+  //   s.parentNode.insertBefore(hm, s);
+  // })();
+  //   `
+  return (
+    <script
+      src="https://hm.baidu.com/hm.js?f09245f6946d90bfd6d5cd6a0eed11c6"
+      defer
+    ></script>
+  )
 }
