4729dad1c3dcec42e73bf5812cec11f45f25b49b
mark <wsjzs@hotmail.com>
2023-08-09 17:54:55 +0800

    feat: host

--- src/lib/fetch/index.ts
+++ src/lib/fetch/index.ts
@@ -1,16 +1,22 @@
 import { Http } from './http'
 
 const requestOpt = {
-  // host: typeof window !== "undefined"
-  //   ? process.env.NODE_ENV === 'production'
-  //     ? 'https://tools.whatfa.com'
-  //     : 'http:localhost:3000'
-  //   : '',
   host:
-    process.env.NODE_ENV === 'production'
-      ? 'https://tools.whatfa.com'
-      : 'http:localhost:3000',
+    typeof window === 'undefined'
+      ? process.env.NODE_ENV === 'production'
+        ? 'https://tools.whatfa.com'
+        : 'http:localhost:3000'
+      : '',
 }
+
+console.log(
+  'use---',
+  typeof window === 'undefined'
+    ? process.env.NODE_ENV === 'production'
+      ? 'https://tools.whatfa.com'
+      : 'http:localhost:3000'
+    : ''
+)
 
 const http = new Http(requestOpt)
 
